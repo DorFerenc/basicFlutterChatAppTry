eefb7b55ce6d3ecf0e6ceec65fee40541eda69c6
DorFerenc <dor.ferenc@s.afeka.ac.il>
2023-09-12 20:44:14 +0300

    imageWITHUpload

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
+
 import 'package:chat_app_try/widgets/user_image_picker.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -20,11 +22,13 @@ class _AuthScreenState extends State<AuthScreen> {
   var _isLogin = true;
   var _enteredEmail = '';
   var _enteredPassword = '';
+  File? _selectedImage;
 
   void _submit() async {
     final isValid = _formKey.currentState!.validate();
 
-    if (!isValid) {
+    if (!isValid || (!_isLogin && _selectedImage == null)) {
+      // TODO show error message ...
       return;
     }
 
@@ -38,7 +42,17 @@ class _AuthScreenState extends State<AuthScreen> {
       } else {
         // signup users
         final userCredentials = await _firebase.createUserWithEmailAndPassword(
-            email: _enteredEmail, password: _enteredPassword);
+            email: _enteredEmail,
+            password: _enteredPassword); //create a new user
+
+        final storageRef = FirebaseStorage.instance
+            .ref()
+            .child('user_images')
+            .child('${userCredentials.user!.uid}.jpg');
+
+        await storageRef.putFile(_selectedImage!);
+        final imageUrl = await storageRef.getDownloadURL();
+        print(imageUrl);
       }
     } on FirebaseAuthException catch (error) {
       if (error.code == 'email-already-in-use') {
@@ -76,7 +90,10 @@ class _AuthScreenState extends State<AuthScreen> {
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       //if in signup then open image picker widget
-                      if (!_isLogin) UserImagePicker(),
+                      if (!_isLogin)
+                        UserImagePicker(onPickImage: (pickedImage) {
+                          _selectedImage = pickedImage;
+                        }),
                       TextFormField(
                         decoration:
                             const InputDecoration(labelText: 'Email Address'),
--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
-  const UserImagePicker({super.key});
+  const UserImagePicker({super.key, required this.onPickImage});
+
+  final void Function(File pickedImage) onPickImage;
 
   @override
   State<UserImagePicker> createState() {
@@ -16,7 +18,11 @@ class _UserImagePickerState extends State<UserImagePicker> {
   File? _pickedImageFile;
 
   void _pickImage() async {
-    final pickedImage = await ImagePicker().pickImage(source: ImageSource.camera, imageQuality: 50, maxWidth: 150,);
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
 
     if (pickedImage == null) {
       return;
@@ -25,6 +31,8 @@ class _UserImagePickerState extends State<UserImagePicker> {
     setState(() {
       _pickedImageFile = File(pickedImage.path);
     });
+
+    widget.onPickImage(_pickedImageFile!);
   }
 
   @override
@@ -34,7 +42,8 @@ class _UserImagePickerState extends State<UserImagePicker> {
         CircleAvatar(
           radius: 40,
           backgroundColor: Colors.grey,
-          foregroundImage: _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
+          foregroundImage:
+              _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
         ),
         TextButton.icon(
             onPressed: _pickImage,
